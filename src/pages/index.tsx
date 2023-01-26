d640f2a287f6f661b220477e34bf221d5af3953e
Kumar Ankush <000.kumarankush@gmail.com>
2023-01-26 12:56:59 +0530

    changes and defined root directory to src

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return <div className="text-[10rem] text-red-500">hello</div>;
+};
+
+export default Home;
