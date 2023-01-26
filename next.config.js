54a83609928a6300696d26b2df2ed50edb4bda6f
Kumar Ankush <000.kumarankush@gmail.com>
2023-01-26 12:56:05 +0530

    added domains for Image

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["s4.anilist.co", "gogocdn.net", "images.weserv.nl"],
+  },
+  i18n: {
+    locales: ["en-US", "fr", "nl-NL", "nl-BE"],
+    defaultLocale: "en-US",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
